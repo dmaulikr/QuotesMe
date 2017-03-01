4917c8d29f7b026e614c24c850c675be58fda1f9
Harry Ng <poripori1030@gmail.com>
2017-03-01 16:02:52 +0800

    second commit

--- QuotesMe/Model.swift
+++ QuotesMe/Model.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class Quotes {
     
+    var quotesArray = [
+        "If you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long. Just figure out what’s next.",
+        "No matter how small you start, start something that matters.",
+        "The credit belongs to those who are actually in the arena, who strive valiantly; who know the great enthusiasums, the great devotions, and spend themselves in a worthy cause; who at best know the triumph of high achievement; and who, at worst, if they fail, fail while daring greatly, so that their place shall never be with those cold and timid souls who know neither victory nor defeat."
+    ]
     
+    var authorArray = ["Steve Jobs", "Brendon Burchard", "Theodore Roosevelt"]
+    
+    var authorImageArray = ["thumb_steve", "thumb_brendon", "thumb_theodore"]
+    
+    var currentIndex = 0
+    
+    
+    func nextQuotes() -> String {
+        if  currentIndex == (quotesArray.count - 1) {
+            currentIndex = 0
+        } else  {
+            currentIndex += 1
+        }
+        let quotes = quotesArray[currentIndex]
+        return quotes
+    }
+    
+    func nextAuthorImage() -> String {
+        let authorImage = authorImageArray[currentIndex]
+        return authorImage
+    }
+    
+    func nextAuthor() -> String {
+        let author = authorArray[currentIndex]
+        return author
+    }
     
 }
--- QuotesMe/QuoteVC.swift
+++ QuotesMe/QuoteVC.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class QuoteVC: UIViewController {
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var inpireMeButton: UIButton!
+    
+    var myQuotes = Quotes()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        inpireMeButton.layer.cornerRadius = 5
+        inpireMeButton.clipsToBounds = true
         
     }
 
-
-
+    @IBAction func inpireMeButtonTapped(_ sender: Any) {
+        updateQuotes()
+    }
+    
+    
+    
+    func updateQuotes() {
+        
+        let quote = myQuotes.nextQuotes()
+        let author = myQuotes.nextAuthor()
+        let authorImg = myQuotes.nextAuthorImage()
+        let image = UIImage(named: authorImg)
+        
+        quoteLabel.text = quote
+        authorName.text = author
+        authorImage.image = image
+    }
 }
 
