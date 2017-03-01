0e3a8e5aec8194cb008cb6356d7cb4360be0a74d
Harry Ng <poripori1030@gmail.com>
2017-03-01 14:08:15 +0800

    initial commit

---
+++ QuotesMe/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  QuotesMe
+//
+//  Created by Harry Ng on 01/03/2017.
+//  Copyright © 2017 Harry Ng. All rights reserved.
+//
+
+import UIKit
+
+class Quotes {
+    
+    
+    
+}
---
+++ QuotesMe/QuoteVC.swift
@@ -0,0 +1,23 @@
+//
+//  QuoteVC.swift
+//  QuotesMe
+//
+//  Created by Harry Ng on 01/03/2017.
+//  Copyright © 2017 Harry Ng. All rights reserved.
+//
+
+import UIKit
+
+class QuoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+
+}
+
